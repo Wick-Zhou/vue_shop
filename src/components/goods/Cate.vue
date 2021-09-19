24726049dad7986f5e7642bbfdc935e7afaa8667
ZHOUwl8 <2573681101@qq.com>
2021-09-19 20:04:50 +0800

    商品分类

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,180 @@
+<template>
+  <div>
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-button type="primary" @click="showAddCateDialog">添加分类</el-button>
+      <tree-table :data="catalist"
+      :columns="columns"
+      :selection-type="false"
+      :expand-type="false"
+      show-index
+      border
+      :show-row-hover="false">
+        <template slot="isOk" slot-scope="scope">
+          <i class="el-icon-circle-check" style="color:lightgreen" v-if="scope.row.cat_deleted===true"></i>
+          <i class="el-icon-circle-close" style="color:red" v-else></i>
+        </template>
+        <template slot="order" slot-scope="scope">
+          <el-tag size="small" v-if="scope.row.cat_level===0">一级</el-tag>
+          <el-tag size="small" type="success" v-else-if="scope.row.cat_level===1">二级</el-tag>
+          <el-tag size="small" type="warning" v-else>三级</el-tag>
+        </template>
+        <template slot="operate">
+          <el-button type="primary" round icon="el-icon-edit" size="small">编辑</el-button>
+          <el-button type="danger" round icon="el-icon-delete" size="small">删除</el-button>
+        </template>
+      </tree-table>
+      <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="queryInfo.pagenum"
+      :page-sizes="[1,2,3,4,5,6,7,8,9,10]"
+      :page-size="queryInfo.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+      </el-pagination>
+    </el-card>
+    <el-dialog
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      @close="addCateDialogClose">
+      <el-form :model="addCateForm" :rules="addCateFormRules" ref="addCateFormRef" label-width="100px">
+        <el-form-item label="分类名称:" prop="cat_name">
+            <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级分类:">
+          <el-cascader
+          v-model="selectKeys"
+          :options="parentCateList"
+          :props="cascaderProps"
+          @change="parentCateChange"
+          clearable>
+          </el-cascader>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      queryInfo: {
+        // 查询参数
+        query: '',
+        // 当前页码
+        pagenum: 1,
+        // 每页显示条数
+        pagesize: 3
+      },
+      catalist: [],
+      total: 0,
+      columns: [
+        { label: '分类名称', prop: 'cat_name' },
+        { label: '是否有效', type: 'template', template: 'isOk' },
+        { label: '排序', type: 'template', template: 'order' },
+        { label: '操作', type: 'template', template: 'operate' }
+      ],
+      addCateDialogVisible: false,
+      addCateForm: {
+        cat_name: '',
+        cat_pid: 0,
+        cat_level: 0
+      },
+      addCateFormRules: {
+        cat_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' }
+        ]
+      },
+      parentCateList: [],
+      cascaderProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children',
+        expandTrigger: 'hover',
+        checkStrictly: 'true'
+      },
+      selectKeys: []
+    }
+  },
+  methods: {
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories', { params: this.queryInfo })
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.catalist = res.data.result
+      this.total = res.data.total
+    },
+    handleSizeChange (newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getCateList()
+    },
+    handleCurrentChange (newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getCateList()
+    },
+    showAddCateDialog () {
+      this.getParentCateList()
+      this.addCateDialogVisible = true
+    },
+    async getParentCateList () {
+      const { data: res } = await this.$http.get('categories', { params: { type: 2 } })
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.parentCateList = res.data
+    },
+    parentCateChange () {
+      console.log(this.selectKeys)
+      if (this.selectKeys.length > 0) {
+        this.addCateForm.cat_pid = this.selectKeys[this.selectKeys.length - 1]
+        this.addCateForm.cat_level = this.selectKeys.length
+      } else {
+        this.addCateForm.cat_pid = 0
+        this.addCateForm.cat_level = 0
+      }
+    },
+    addCateDialogClose () {
+      this.$refs.addCateFormRef.resetFields()
+      this.selectKeys = []
+      this.addCateForm.cat_pid = 0
+      this.addCateForm.cat_level = 0
+    },
+    addCate () {
+      this.$refs.addCateFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('categories', this.addCateForm)
+        if (res.meta.status !== 201) return this.$message.error(res.meta.msg)
+        this.$message.success(res.meta.msg)
+        this.addCateDialogVisible = false
+        this.getCateList()
+      })
+    }
+  },
+  created () {
+    this.getCateList()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+    .el-card{
+        margin-top: 20px;
+    }
+    .el-button{
+        margin-bottom: 20px;
+    }
+    .el-pagination{
+      margin-top: 20px;
+    }
+    .el-cascader{
+      width: 100%;
+    }
+</style>
--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -6,60 +6,60 @@
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-        <el-button type="primary">添加角色</el-button>
-        <el-table :data="roleslist" style="width: 100%" border stripe>
-            <!-- 第一列可展开栏 -->
-            <!-- 展开栏的整体布局
-                    三层权限一共分为2列(el一共分为24份)：
-                        第一列为第一层权限(占5份)
-                        第二列为2、3层权限(占剩余的19份)
-                            把这部分又分为24份
-                            第二列当中的第二层权限分到第一列(占其中的7份)
-                            第二列当中的第三层权限分到第二列(占其中17份)
-            -->
-            <el-table-column type="expand">
-                <template slot-scope="scope">
-                    <el-row v-for="(item,index) in scope.row.children" :key="item.id"
-                    :class="['tagbottom',index===0?'tagtop':'','vcenter']">
-                        <el-col :span="5">
-                            <el-tag closable @close="removeRight(scope.row,item.id)">
-                                {{item.authName}}
-                                <i class="el-icon-caret-right"></i>
-                            </el-tag>
-                        </el-col>
-                        <el-col :span="19">
-                            <el-row v-for="(item2,index2) in item.children" :key="item2.id"
-                            :class="[index2===0?'':'tagtop','vcenter']">
-                                <el-col :span="7">
-                                    <el-tag type="success" closable @close="removeRight(scope.row,item2.id)">
-                                        {{item2.authName}}
-                                        <i class="el-icon-caret-right"></i>
-                                    </el-tag>
-                                </el-col>
-                                <el-col :span="17">
-                                    <el-tag type="warning"
-                                    v-for="(item3) in item2.children" :key="item3.id"
-                                    closable @close="removeRight(scope.row,item3.id)">
-                                        {{item3.authName}}
-                                        <i class="el-icon-caret-right"></i>
-                                    </el-tag>
-                                </el-col>
-                            </el-row>
-                        </el-col>
-                    </el-row>
-                </template>
-            </el-table-column>
-            <el-table-column type="index" label="#"></el-table-column>
-            <el-table-column prop="roleName" label="角色名称"></el-table-column>
-            <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
-            <el-table-column prop="level" label="操作">
-                <template slot-scope="scope">
-                    <el-button type="primary" round icon="el-icon-edit" size="mini">编辑</el-button>
-                    <el-button type="danger" round icon="el-icon-delete" size="mini">删除</el-button>
-                    <el-button type="warning" round icon="el-icon-setting" size="mini" @click="showSetRigthsDialog(scope.row)">分配权限</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+      <el-button type="primary">添加角色</el-button>
+      <el-table :data="roleslist" style="width: 100%" border stripe>
+        <!-- 第一列可展开栏 -->
+        <!-- 展开栏的整体布局
+            三层权限一共分为2列(el一共分为24份)：
+              第一列为第一层权限(占5份)
+              第二列为2、3层权限(占剩余的19份)
+                把这部分又分为24份
+                第二列当中的第二层权限分到第一列(占其中的7份)
+                第二列当中的第三层权限分到第二列(占其中17份)
+        -->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row v-for="(item,index) in scope.row.children" :key="item.id"
+            :class="['tagbottom',index===0?'tagtop':'','vcenter']">
+              <el-col :span="5">
+                  <el-tag closable @close="removeRight(scope.row,item.id)">
+                      {{item.authName}}
+                      <i class="el-icon-caret-right"></i>
+                  </el-tag>
+              </el-col>
+              <el-col :span="19">
+                <el-row v-for="(item2,index2) in item.children" :key="item2.id"
+                :class="[index2===0?'':'tagtop','vcenter']">
+                  <el-col :span="7">
+                    <el-tag type="success" closable @close="removeRight(scope.row,item2.id)">
+                      {{item2.authName}}
+                      <i class="el-icon-caret-right"></i>
+                    </el-tag>
+                  </el-col>
+                  <el-col :span="17">
+                    <el-tag type="warning"
+                    v-for="(item3) in item2.children" :key="item3.id"
+                    closable @close="removeRight(scope.row,item3.id)">
+                      {{item3.authName}}
+                      <i class="el-icon-caret-right"></i>
+                    </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column prop="roleName" label="角色名称"></el-table-column>
+        <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
+        <el-table-column prop="level" label="操作">
+          <template slot-scope="scope">
+            <el-button type="primary" round icon="el-icon-edit" size="mini">编辑</el-button>
+            <el-button type="danger" round icon="el-icon-delete" size="mini">删除</el-button>
+            <el-button type="warning" round icon="el-icon-setting" size="mini" @click="showSetRigthsDialog(scope.row)">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
     <!-- 选择 编辑权限对话框 -->
     <el-dialog
@@ -78,8 +78,8 @@
         ref="treeRef">
         </el-tree>
         <span slot="footer" class="dialog-footer">
-            <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="confirmSelectRights">确 定</el-button>
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="confirmSelectRights">确 定</el-button>
         </span>
     </el-dialog>
   </div>
